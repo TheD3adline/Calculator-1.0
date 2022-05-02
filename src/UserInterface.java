cc158fa75230f5c73615be25fb5a249a55d812c1
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-05-02 15:09:53 +0200

    Optimized Main.java, implemented loops for every step so the program doesn't need to be terminated upon a single invalid input anymore

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,23 +1,20 @@
 public class UserInterface {
     public static void printUI() {  //the class and static method to print the UI to console, later to be expanded to properly read and display all relevant data
         System.out.println("************************************************************************");
-        System.out.println("*                                                                       ");
-        System.out.println("*       + Add                                 1.) " + Memory.readMem(0));
-        System.out.println("*       - Subtract                            2.) " + Memory.readMem(1));
-        System.out.println("*       * Multiply                            3.) " + Memory.readMem(2));
-        System.out.println("*       / Divide                              4.) " + Memory.readMem(3));
-        System.out.println("*       m Memory                              5.) " + Memory.readMem(4));
-        System.out.println("*                                             6.) " + Memory.readMem(5));
-        System.out.println("*                                             6.) " + Memory.readMem(6));
-        System.out.println("*       y Another Calculation                 8.) " + Memory.readMem(7));
-        System.out.println("*       x Exit Program                        9.) " + Memory.readMem(8));
-        System.out.println("*                                            10.) " + Memory.readMem(9));
-
-        System.out.println("*                                                                       ");
-        System.out.println("*       69 + 420 = 489                                                  ");
-
-
-        System.out.println("*                                                                       ");
+        System.out.println("                                                                        ");
+        System.out.println("        + Add                                 1.) " + Memory.readMem(0));
+        System.out.println("        - Subtract                            2.) " + Memory.readMem(1));
+        System.out.println("        * Multiply                            3.) " + Memory.readMem(2));
+        System.out.println("        / Divide                              4.) " + Memory.readMem(3));
+        System.out.println("        m Memory                              5.) " + Memory.readMem(4));
+        System.out.println("                                              6.) " + Memory.readMem(5));
+        System.out.println("                                              6.) " + Memory.readMem(6));
+        System.out.println("        y Another Calculation                 8.) " + Memory.readMem(7));
+        System.out.println("        x Exit Program                        9.) " + Memory.readMem(8));
+        System.out.println("                                             10.) " + Memory.readMem(9));
+        System.out.println("                                                                        ");
+        System.out.println("        69 + 420 = 489                                                  ");
+        System.out.println("                                                                        ");
         System.out.println("************************************************************************");
     }
 }
