a8a08cd53b227b97f7f538e84f1ac797e2257adb
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-05-02 13:43:02 +0200

    Optimized Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
             System.out.println("Enter number 1 or m to access memory (or x to exit program): ");
             input = sc.next();  //input is always of String to allow for - number 1 > operator > number 2 - input order like in a real calculator
             switch(input) {
-                case "m": break;    //the switch branching to access the memory array and inject one of its double data as number 1 to be calculated, to be implemented
+                case "m": number1 = 0.0; number2 = 0.0; break;    //the switch branching to access the memory array and inject one of its double data as number 1 to be calculated, to be implemented
                 case "x": System.out.println("Exiting program..."); System.exit(0);     //x branching to exit program
                 default: if(Main.isNumeric(input) == true) {        //checks if input is numeric and then assigns it to number 1 variable
                     number1 = Double.parseDouble(input);
@@ -37,23 +37,31 @@ public class Main {
             System.out.println("Enter number 2 or m to access memory (or x to exit program): ");    //next step is number 2 input, works in the same way as number 1 process with one exception...
             input = sc.next();
             switch(input) {
-                case "m": break;
+                case "m": number1 = 0.0; number2 = 0.0; break;
                 case "x": System.out.println("Exiting program..."); System.exit(0);
                 default: if(Main.isNumeric(input) == true) {        //it has a more intricate if branching within default of the switch case to perform the specified calculations and save the result in the result variable and the memory array
                     number2 = Double.parseDouble(input);
                     if(operator.equals("+")) {
                         result = Calculator.calcAdd(number1, number2);
                         Memory.writeMem(result);
+                        UserInterface.printUI();
+                        System.out.println("The result is: " + result);
                     } else if(operator.equals("-")) {
                         result = Calculator.calcSubtract(number1, number2);
                         Memory.writeMem(result);
+                        UserInterface.printUI();
+                        System.out.println("The result is: " + result);
                     } else if(operator.equals("*")) {
                         result = Calculator.calcMultiply(number1, number2);
                         Memory.writeMem(result);
+                        UserInterface.printUI();
+                        System.out.println("The result is: " + result);
                     } else {
                         try {
                             result = Calculator.calcDivide(number1, number2);
                             Memory.writeMem(result);
+                            UserInterface.printUI();
+                            System.out.println("The result is: " + result);
                         } catch(DivideThroughZeroException e) {
                             System.out.println(e.getMessage());
                             result = 0.0;
@@ -64,9 +72,7 @@ public class Main {
                     System.exit(0);
                 }
             }
-            UserInterface.printUI();
-            System.out.println("The result is: " + result);         //shows the result and asks if the program is to be exited (or terminated, remember to loop that later) or if another calculation loop is to be performed
-            System.out.println("Do you want to do another calculation? Press y for yes or x to exit program: ");
+            System.out.println("Do you want to perform another calculation? Press y for yes or x to exit program: ");        //Asks if the program is to be exited (or terminated, remember to loop that later) or if another calculation loop is to be performed
             input = sc.next();
             switch(input) {
                 case "y": System.out.println("Doing another calculation..."); break;
