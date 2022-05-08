5e26de506382e0e909fed07f5862be82d9de2d81
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-05-08 20:03:45 +0200

    Finished my first little app, now on to bigger things :)

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ First real practice project to consolidate what I've learned there.
 A very basic calculator console application with 10 memory slots for the latest results.
 Coded in a way that makes future extensions easy to do.
 Humble beginnings...
+
+Update May 8th 2022
+Finished my little calculator app, learned a lot about basic console program flow control and how a scanner behaves
+when a single input prompt must allow for different types of data to be read and process them accordingly.
+With a scanner like that I found out that it seems to be best to just read the data into a String and from there
+make junctions in the program flow depending on whether the input was strictly numeric via an .isNumeric()
+boolean return method utilizing for example the Double.parseDouble(String) or Integer.parseInt(String) methods
+of the corresponding Wrapper classes. Other approaches I've tried lead to weird results, trying to .nextDouble()
+or .nextInt() from a scanner prompt will accept strictly a numeric input, or else a type mismatch exception is thrown
+and the program terminated if left alone. And if the exception is handled the previous input seems to be just queued
+and the next time the code asks for an input it will take the queued one instead of issuing a prompt, leading to bugs
+in program flow. Also learned how to customize a memory array and a proper algorithm to write the results of the
+calculations into the array in the order I want it to. Next up Tic Tac Toe.
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {            //First real practice project, a little calculator app, only the 4 basic math operations in a console environment,
                                                         //but with a custom memory array that saves the last 10 results and a function to inject them as the numbers for calculation.
-        String input;                                   ////I'm sure it's super messy and way too intricate, but it's my first real own app...
+        String input;                                   //I'm sure it's super messy and way too intricate, but it's my first real own app...
         String operator = "+";
         String memInput;
         int memSelect;
@@ -19,14 +19,14 @@ public class Main {
 
         while(true) {
             UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
-            while(loopNum1 == true) {
+            while(loopNum1) {
                 System.out.println("Enter number 1 or m to access memory (or x to exit program): ");
                 input = sc.next();  //input is always of String to allow for - number 1 > operator > number 2 - input order like in a real calculator and the possibility to access memory or exit the program at any relevant step
                 switch(input) {
-                    case "m":   //the switch branching to access the memory array and inject one of its double data as number 1 to be calculated, to be implemented
+                    case "m":   //the switch branching to access the memory array and inject one of its double data as number 1 to be calculated
                         System.out.println("Enter memory slot for injection as number 1 (1, 2, 3, 4, 5, 6, 7, 8, 9, or 10): ");
                         memInput = sc.next();
-                        if(Main.isNumericInt(memInput) == true) {
+                        if(Main.isNumericInt(memInput)) {
                             memSelect = Integer.parseInt(memInput);
                             if((memSelect >= 1) && (memSelect <= 10)) {
                                 number1 = Memory.readMem(memSelect - 1);
@@ -38,7 +38,7 @@ public class Main {
                         }
                         break;
                     case "x": System.out.println("Exiting program..."); System.exit(0);     //x branching to exit program
-                    default: if(Main.isNumeric(input) == true) {        //checks if input is numeric and then assigns it to number 1 variable, ends the loop when condition passed
+                    default: if(Main.isNumeric(input)) {        //checks if input is numeric and then assigns it to number 1 variable, ends the loop when condition passed
                                 loopNum1 = false;
                                 number1 = Double.parseDouble(input);
                             } else {
@@ -47,7 +47,7 @@ public class Main {
                 }
             }
             UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
-            while(loopOperator == true) {
+            while(loopOperator) {
                 System.out.println("Enter operator, + - * / (or x to exit program): ");     //next step is the question for the operator
                 operator = sc.next();
                 switch(operator) {
@@ -57,16 +57,20 @@ public class Main {
                 }
             }
             UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
-            while(loopNum2 == true) {
+            while(loopNum2) {
                 System.out.println("Enter number 2 or m to access memory (or x to exit program): ");    //next step is number 2 input, works in the same way as number 1 process with one exception cont...
                 input = sc.next();
                 switch(input) {
-                    case "m": loopNum2 = false; number1 = 0.0; number2 = 0.0; break;            //the switch branching to access the memory array and inject one of its double data as number 2 to be calculated, to be implemented
-                    case "x": System.out.println("Exiting program..."); System.exit(0);
-                    default: if(Main.isNumeric(input) == true) {        //cont... it has a more intricate if branching within default of the switch case to perform the specified calculations and save the result in the result variable and the memory array
-                                number2 = Double.parseDouble(input);            //writes the input into number2 variable and then executes the previously selected arithmetic operation with the previously selected operator via this if-else-if-else branching...
+                    case "m":           //the switch branching to access the memory array and inject one of its double data as number 2 to be calculated
+                        System.out.println("Enter memory slot for injection as number 1 (1, 2, 3, 4, 5, 6, 7, 8, 9, or 10): ");
+                        memInput = sc.next();
+                        if(Main.isNumericInt(memInput)) {
+                            memSelect = Integer.parseInt(memInput);
+                            if((memSelect >= 1) && (memSelect <= 10)) {
+                                number2 = Memory.readMem(memSelect - 1);
+                                System.out.println("Successfully injected " + Memory.readMem(memSelect - 1) + " as number 2!");
                                 if(operator.equals("+")) {
-                                    loopNum2 = false;                           //and then ends while loopNum2
+                                    loopNum2 = false;
                                     result = Calculator.calcAdd(number1, number2);
                                     Memory.writeMem(result);
                                     UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
@@ -96,12 +100,51 @@ public class Main {
                                         result = 0.0;
                                     }
                                 }
+                            }
+                        } else {
+                            System.out.println("Error, invalid input...");
+                        }
+                        break;
+                    case "x": System.out.println("Exiting program..."); System.exit(0);
+                    default: if(Main.isNumeric(input)) {                        //cont... it has a more intricate if branching within default of the switch case to perform the specified calculations and save the result in the result variable and the memory array
+                                number2 = Double.parseDouble(input);            //writes the input into number2 variable and then executes the previously selected arithmetic operation with the previously selected operator via this if-else-if-else branching...
+                                if(operator.equals("+")) {
+                                    loopNum2 = false;                           //and then ends while loopNum2
+                                    result = Calculator.calcAdd(number1, number2);
+                                    Memory.writeMem(result);
+                                    UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
+                                    System.out.println("The result is: " + result);
+                                } else if(operator.equals("-")) {
+                                    loopNum2 = false;
+                                    result = Calculator.calcSubtract(number1, number2);
+                                    Memory.writeMem(result);
+                                    UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
+                                    System.out.println("The result is: " + result);
+                                } else if(operator.equals("*")) {
+                                    loopNum2 = false;
+                                    result = Calculator.calcMultiply(number1, number2);
+                                    Memory.writeMem(result);
+                                    UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
+                                    System.out.println("The result is: " + result);
+                                } else {
+                                    try {
+                                        result = Calculator.calcDivide(number1, number2);           //try-catch block to throw an exception if a division by 0 is attempted
+                                        loopNum2 = false;
+                                        Memory.writeMem(result);
+                                        UserInterface.printUI(loopNum1, loopOperator, loopNum2, loopFinal, number1, operator, number2, result);
+                                        System.out.println("The result is: " + result);
+                                    } catch(DivideByZeroException e) {
+                                        System.out.println(e.getMessage());
+                                        loopNum2 = false;
+                                        result = 0.0;
+                                    }
+                                }
                             } else {
                                 System.out.println("Error, invalid input...");  //or if an invalid input is made maintains the loop for this step to ask again for a valid input
                             }
                 }
             }
-            while(loopFinal == true) {
+            while(loopFinal) {
                 System.out.println("Do you want to perform another calculation? Press y for yes or x to exit program: ");        //Asks if the program is to be exited or if another calculation loop is to be performed
                 input = sc.next();
                 switch(input) {
@@ -125,7 +168,7 @@ public class Main {
             return false;
         }
     }
-    public static boolean isNumericInt(String str) {
+    public static boolean isNumericInt(String str) {    //method to check if input for memory selection prompt is an integer or not
         try {
             Integer.parseInt(str);
             return true;
